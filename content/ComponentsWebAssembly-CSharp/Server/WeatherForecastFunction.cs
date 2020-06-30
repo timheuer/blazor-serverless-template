2b5c26b6935a6838e19a78e446353c6840bc3ddf
Tim Heuer <tim@timheuer.com>
2020-06-30 08:57:28 -0700

    Initial

---
+++ content/ComponentsWebAssembly-CSharp/Server/WeatherForecastFunction.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+using BlazorStaticWebApp.Shared;
+
+namespace BlazorStaticWebApp.Server
+{
+    public static class WeatherForecastFunction
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [FunctionName("WeatherForecast")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var rng = new Random();
+            return new OkObjectResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray());
+        }
+    }
+}
